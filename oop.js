4ddf51277e29e3347f6a7769a7d2a32de9ad3a28
Admin <vanpho02@gmail.com>
2017-03-29 18:13:47 +0700

    xong

---
+++ oop.js
@@ -0,0 +1,28 @@
+// const khoa = {
+//     name: 'Khoa Pham',
+//     age: 30
+// };
+
+// console.log(khoa);
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.show = function() {
+        console.log('Hello, I am ' + this.name);
+    }
+    this.incrAge = function() {
+        this.age++;
+    }
+    this.setAge = function(newAge) {
+        this.age = newAge;
+    }
+}
+
+const khoa = new Person('Khoa', 30);
+const huong = new Person('Huong', 29);
+// console.log(khoa.name + khoa.age);
+khoa.incrAge();
+khoa.incrAge();
+khoa.setAge(60);
+console.log(khoa);
