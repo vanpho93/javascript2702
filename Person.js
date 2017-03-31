4bd86947e91747f07d13bd90139a53863c983c1d
Admin <vanpho02@gmail.com>
2017-03-31 18:20:38 +0700

    sort

--- Person.js
+++ Person.js
@@ -17,14 +17,18 @@ const tun = new Person('Tun', 160, 18);
 const arrPerson = [teo, ti, tun];
 
 // for (let i = 0; i < arrPerson.length; i++) {
-//     arrPerson[i].show();
-// }
+    //     arrPerson[i].show();
+    // }
+    //some, every
+    // arrPerson.forEach(e => e.show());
+    // const isHighEnough = arrPerson.some(person => person.height > 190);
+    // const isHighEnough = arrPerson.every(person => person.age > 3);
+    // console.log(isHighEnough);
 
-//some, every
 
-// arrPerson.forEach(e => e.show());
-
-// const isHighEnough = arrPerson.some(person => person.height > 190);
-const isHighEnough = arrPerson.every(person => person.age > 3);
-
-console.log(isHighEnough);
+const a = arrPerson.find(e => e.name === 'Ti');
+    // console.log(a);
+    // findIndex
+    //filter
+    // const arr15 = arrPerson.filter(e => console.log(e.name));
+    // console.log(arr15);
---
+++ sort.js
@@ -0,0 +1,6 @@
+const arrNum = [4, 2, 8, 1, 3, 10];
+
+// const a = arrNum.push(4);
+arrNum.sort((a, b) => a - b);
+// console.log(arrNum);
+console.log(arrNum);
